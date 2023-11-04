105e4156bd0720f73797b3f869c8389ccb6e0383
abautista <alexjav.12@hotmail.es>
2023-11-04 16:56:37 -0600

    add the products using js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData
+  return (shop.innerHTML = candyData
     .map((x) => {
       let { id, name, price, desc, img } = x;
       let search = basket.find((x) => x.id === id) || [];
@@ -16,11 +16,11 @@ let generateShop = () => {
           <div class="price-quantity">
             <h2>$ ${price}</h2>
             <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <i class="bi bi-dash-lg"></i>
               <div id=${id} class="quantity">
               ${search.item === undefined ? 0 : search.item}
               </div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+              <i class="bi bi-plus-lg"></i>
             </div>
           </div>
         </div>
