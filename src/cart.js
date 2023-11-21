03b734b34ec1bf1071ad8fed119e09305806ed78
abautista <alexjav.12@hotmail.es>
2023-11-20 20:09:18 -0600

    add the calculation and the update functions to the cart.js file

--- src/cart.js
+++ src/cart.js
@@ -2,7 +2,12 @@ let label = document.getElementById("label");
 let ShoppingCart = document.getElementById("shopping-cart");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let generateCartIcons = () => {
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+let generateCartItems = () => {
   if (basket.length !== 0) {
     return (ShoppingCart.innerHTML = basket
       .map((x) => {
@@ -27,9 +32,9 @@ let generateCartIcons = () => {
             </div>
 
             <div class="buttons">
-                <i class="bi bi-dash-lg"></i>
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                 <div id=${id} class="quantity">${item}</div>
-                <i class="bi bi-plus-lg"></i>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
 
             <h3>$ ${item * search.price}</h3>
@@ -49,4 +54,45 @@ let generateCartIcons = () => {
   }
 };
 
-generateCartIcons();
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((item) => item.id === selectedItem.id);
+  // if we don't find the object that we are searching, only then we push this object
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((item) => item.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0); // the moment the quantity is 0, this line will run
+  //   Once it's done filtering the data, this will rerender the cards using the generateCards function
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
